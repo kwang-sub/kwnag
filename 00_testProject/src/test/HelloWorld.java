904e7a9033eba2da54579f7858197b4fe65a546c
kwang-sub <74045087+kwang-sub@users.noreply.github.com>
2021-03-20 13:57:28 +0900

    Add files via upload

---
+++ 00_testProject/src/test/HelloWorld.java
@@ -0,0 +1,15 @@
+package test;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("헬로우 월드!!");
+		System.out.println("다들 맛점하세요~");
+//		점심 맛있게 드셨나요?
+				/*5교시
+				6교시
+				7교시*/
+		
+		//:한줄주석
+//		/* */:범위주석
+	}
+}
